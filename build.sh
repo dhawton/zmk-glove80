2c9031f21b24d82aae4c06102e9d917838d5d5db
Daniel Hawton <daniel@hawton.org>
2024-06-20 00:12:34 -0600

    setup glove80 keymaps

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 IMAGE=glove80-zmk-config-docker
 BRANCH="${1:-main}"
 
-docker build -t "$IMAGE" .
+docker buildx build -t "$IMAGE" .
 docker run --rm -v "$PWD:/config" -e UID="$(id -u)" -e GID="$(id -g)" -e BRANCH="$BRANCH" "$IMAGE"
